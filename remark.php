80e54ad245cebd8c8b92471ea8a7ce070d22f88b
下载统计 <44078658+shanslang@users.noreply.github.com>
2019-11-13 15:21:23 +0800

    Update remark.php

--- remark.php
+++ remark.php
@@ -1,2 +1,7 @@
 先平滑启动：nohup /www/server/php/72/bin/php  /www/wwwroot/Clog/HttpClog.php  >  /www/wwwroot/Clog/HttpClog.txt  &
 服务，再通过nginx代理，要建client_log文件夹
+
+location / {
+        index index.html index.htm;     
+        proxy_pass http://127.0.0.1:8501;
+    }
